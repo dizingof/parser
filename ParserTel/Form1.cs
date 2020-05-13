60fa18a9a85406dfb519570d7700c9154a12ab2d
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-14 00:47:05 +0300

    add parser tel

---
+++ ParserTel/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserTel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start();
+
+        }
+
+        public async Task Start()
+        {
+            string path = @"C:\SomeDir\hta.txt";
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    richTextBox1.Text += line;
+                }
+            }
+        }
+    }
+}
